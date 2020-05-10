a16ce57323f6e72dcf2223f3bf8a06595b3712f6
mujahid-ali <mujahidali0611@gmail.com>
2020-05-10 16:51:20 +0530

    Error handling

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,11 +10,12 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedpostid: null
+        selectedpostid: null,
+        error: false
     }
     //Best place for http get
     componentDidMount () {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/postssss')
             .then(response => {
                 const posts = response.data.slice(0,4);
                 const updatedPosts = posts.map(post => {
@@ -25,6 +26,10 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
                 //console.log(response);
+            })
+            .catch(error => {
+                //console.log(error);
+                this.setState({error: true});
             });
     }
 
@@ -33,14 +38,17 @@ class Blog extends Component {
 
     }
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id}
-                title={post.title} 
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)}
-                />;
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>; 
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id}
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}
+                    />;
+            });
+        }
         return (
             <div>
                 <section className="Posts">
